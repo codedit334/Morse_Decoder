43ff891550972c7939c1b3214c4ae3ee765c1b74
codedit334 <oussama7elabdioui@gmail.com>
2023-05-10 15:23:48 +0100

    Fix linter errors

--- decoder.rb
+++ decoder.rb
@@ -1,37 +1,38 @@
+morse_code = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
 def decode_char(code)
-  morse_code = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
-  }
   morse_code[code]
 end
 
 def decode_word(word)
-  word.split(" ").map { |char| decode_char(char) }.join
+  word.split.map { |char| decode_char(char) }.join
 end
 
 def decode(morse_message)
@@ -39,3 +40,5 @@ def decode(morse_message)
   decoded_words = morse_words.map { |morse_word| decode_word(morse_word) }
   decoded_words.join(' ')
 end
+
+puts morse_code
