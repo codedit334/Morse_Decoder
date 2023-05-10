2d258bd9193efdc2687fa34d98d6ad72be3df7c5
zeff96 <zeffo1975@gmail.com>
2023-05-10 16:43:40 +0300

    Add decode entire message method.

--- decoder.rb
+++ decoder.rb
@@ -1,11 +1,20 @@
-morse_code = {
-  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
-}
 
 def decode_char(code)
-  morse_code[code]
+  morse_code = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }
+  morse_code_arr = morse_code.to_s
+  morse_code_arr[code]
 end
 
 def decode_word(word)
   word.split(" ").map { |char| decode_char(char) }.join
 end
+
+def decode(morse_message)
+  morse_words = morse_message.split('   ')
+  decoded_words = morse_words.map { |morse_word| decode_word(morse_word) }
+  decoded_words.join(' ')
+end
+
+decode_char(".-")
